67194c6e6cde780a2c4ad224106c305bbed6d977
Meinolf Droste <mdwp.post@gmail.com>
2018-05-02 14:39:00 +0200

    config changed

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', href: `/logo.png` }],
         ['link', { rel: 'manifest', href: '/manifest.json' }],
-        ['link', { rel: 'canonical', href: 'https://vuepress-de.netlify.com' }],
+        ['link', { rel: 'canonical', href: 'https://www.vuepress.de' }],
         ['meta', { name: 'theme-color', content: '#3eaf7c' }],
         ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
         ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
