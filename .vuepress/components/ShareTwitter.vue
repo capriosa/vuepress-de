36fd0541fbb3e6d272854d58df05b0c0645651c2
Meinolf Droste <mdwp.post@gmail.com>
2018-05-10 01:18:21 +0200

    twitter share component added

--- .vuepress/components/ShareTwitter.vue
+++ .vuepress/components/ShareTwitter.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="share">
-<h4>Teile diesen Artikel auf Twitter!</h4>
+<h4>Teile dies auf Twitter!</h4>
 <a :href="twitterUrl" target="_blank" class="resp-sharing-button__link">
   
   <div class="resp-sharing-button resp-sharing-button--twitter resp-sharing-button--large">
