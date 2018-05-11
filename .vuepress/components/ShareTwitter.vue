22cfee246821f86acc813e4b503d694e0b558dad
Meinolf Droste <meinolf.droste@mdwp.de>
2018-05-11 14:54:43 +0200

    code style changed

--- .vuepress/components/ShareTwitter.vue
+++ .vuepress/components/ShareTwitter.vue
@@ -1,6 +1,8 @@
 <template>
+<div class="share-head">
+<h4>Teile diesen Artikel!</h4>
 <div class="share">
-<h4>Teile dies auf Twitter!</h4>
+
 <a :href="twitterUrl" target="_blank" class="resp-sharing-button__link">
   
   <div class="resp-sharing-button resp-sharing-button--twitter resp-sharing-button--large">
@@ -10,6 +12,17 @@
   </div>
     
 </a>
+
+<a :href="facebookUrl" target="_blank" class="resp-sharing-button__link">
+<div class="resp-sharing-button resp-sharing-button--facebook resp-sharing-button--large">
+    <div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solid">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.77 7.46H14.5v-1.9c0-.9.6-1.1 1-1.1h3V.5h-4.33C10.24.5 9.5 3.44 9.5 5.32v2.15h-3v4h3v12h5v-12h3.85l.42-4z"/></svg>
+    </div>
+  </div>
+</a>
+
+
+</div>
 </div>
 </template>
 
@@ -26,8 +39,11 @@ export default {
 computed: {
     twitterUrl() {
       return 'https://twitter.com/intent/tweet/?text=' + this.sharetext + '&' + 'url=https://vuepress.de/' + this.url;
+    },
+    facebookUrl() {
+      return 'https://facebook.com/sharer/sharer.php?u=' + 'https://vuepress.de/' + this.url; 
     }
+    
   }
 }
 </script>
-  
