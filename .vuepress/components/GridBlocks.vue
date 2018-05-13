c9086a2ca1b2e8d14e5db15a4daaca268870c63d
Meinolf Droste <mdwp.post@gmail.com>
2018-05-13 14:27:11 +0200

    added component Grid Blocks

---
+++ .vuepress/components/GridBlocks.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="flex-wrapper">
+  <div>{{ feature1 }}</div> 
+  <div>{{ feature2 }}</div> 
+  <div>{{ feature3 }}</div>
+</div>
+
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+  feature1: {
+    type: String
+  },
+  feature2: {
+    type: String
+  },
+  feature3: {
+    type: String
+  }
+  }
+}
+</script>
