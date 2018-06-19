a76533b9ca15631c883344fcb81530784675207f
Meinolf Droste <meinolf.droste@mdwp.de>
2018-06-19 16:23:32 +0200

    bugfix

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Ich bin Meinolf Droste, Frontend-Entwickler und Inhaber von mdwp - Digital Consulting & Services. Hier schreibe ich über Vue.js und Vuepress.",
     head: [
         ['link', { rel: 'icon', href: `/logo.png` }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['link', { rel: 'manifest', href: 'manifest.json' }],
         ['meta', { name: 'theme-color', content: '#4527a0' }],
         ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
         ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
